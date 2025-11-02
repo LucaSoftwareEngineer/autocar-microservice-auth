c22baab4abde9061ac5571f62480053be5ba5fe6
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-11-02 22:34:34 +0100

    initial commit, configurato application.properties con connessione al db

---
+++ src/main/java/autocar/microservice/MicroserviceAuthApplication.java
@@ -0,0 +1,13 @@
+package autocar.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAuthApplication.class, args);
+	}
+
+}
