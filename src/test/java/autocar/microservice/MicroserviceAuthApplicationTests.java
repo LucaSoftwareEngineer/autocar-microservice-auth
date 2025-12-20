a95b8f74dc28c7f5db6a8b38207f79ec38cff075
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-12-20 13:42:07 +0100

    implementato configurazioni per eserguire i test su piattaforma CI/CD Jenkins

--- src/test/java/autocar/microservice/MicroserviceAuthApplicationTests.java
+++ src/test/java/autocar/microservice/MicroserviceAuthApplicationTests.java
@@ -1,9 +1,7 @@
 package autocar.microservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MicroserviceAuthApplicationTests {
 
 	@Test
