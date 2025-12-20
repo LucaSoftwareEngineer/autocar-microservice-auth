54a6f0864d1a1305e17fa0246ab57253e8f40835
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-12-20 12:31:49 +0100

    implementato unit test di userController.registerUser()

--- src/test/java/autocar/microservice/controllers/UserControllerTest.java
+++ src/test/java/autocar/microservice/controllers/UserControllerTest.java
@@ -1,10 +1,19 @@
 package autocar.microservice.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
 
+import autocar.microservice.dto.RegisterRequest;
+import autocar.microservice.dto.RegisterResponse;
+import autocar.microservice.exceptions.InvalidRole;
 import autocar.microservice.services.UserService;
 
 public class UserControllerTest {
@@ -19,5 +28,21 @@ public class UserControllerTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
+	
+	@Test
+	public void registerUserTest() throws InvalidRole {
+		
+		RegisterRequest req = new RegisterRequest();
+		req.setEmail("test");
+		req.setPassword("test");
+		
+		RegisterResponse res = new RegisterResponse();
+		res.setEmail(req.getEmail());
+		
+		when(userService.registerUser(any(RegisterRequest.class))).thenReturn(res);
+		
+		assertEquals(userController.registerUser(req), ResponseEntity.ok().body(res));
+	}
+	
 	
 }
